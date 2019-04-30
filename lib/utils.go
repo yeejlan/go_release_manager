ad9de7e882ee84200c3cbf27ec754d692702b412
louis yu <yf_hello@163.com>
2019-04-30 13:33:45 +0800

    move utils

--- lib/utils.go
+++ lib/utils.go
@@ -1,11 +1,17 @@
-package utils
+package lib
 
 import (
 	"github.com/yeejlan/maru"
 	"strings"
 )
 
-func GetClientIP(r *http.Request) string {
+var (
+	Utils = &utils{}
+)
+
+type utils struct{}
+
+func (this *utils) GetClientIP(r *http.Request) string {
 	ipAddress := r.Header.Get("X-Forwarded-For")
 	if ipAddress == "" {
 		ipAddress = r.RemoteAddr
